a4356353098c29052b00c842c76ac83a80d5188a
Dr.Morais <zinjoao254@gmail.com>
2024-05-03 08:32:25 -0300

    database

---
+++ config/database.php
@@ -0,0 +1,3 @@
+<?php
+
+$con = new PDO("mysql:host=database-epgal.cinrt0cz5hud.us-east-1.rds.amazonaws.com;dbname=anime", "admin", "epgalcrede17") or die("Failed to connect to MySQL: " . mysqli_connect_error());
